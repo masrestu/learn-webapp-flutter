6a76dbc616bd40fd9dc19fe99a13a63dfee9c31b
masrestu <n.rizkiawan@gmail.com>
2024-07-09 07:28:53 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping Cart',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const WebViewDemo(),
+    );
+  }
+}
+
+class WebViewDemo extends StatefulWidget {
+  const WebViewDemo({super.key});
+
+  @override
+  State<WebViewDemo> createState() => _WebViewDemoState();
+}
+
+class _WebViewDemoState extends State<WebViewDemo> {
+  WebViewController controller = WebViewController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(NavigationDelegate(
+        onProgress: (progress) => {},
+        onPageStarted: (url) => {},
+        onPageFinished: (url) => {},
+      ))
+      // ..loadRequest(Uri.parse('https:/cart.masrestu.online'));
+      ..loadRequest(Uri.parse('https:/cart.masrestu.online'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: true,
+      child: WebViewWidget(controller: controller),
+    );
+  }
+}
